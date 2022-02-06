3249b628149085aabef4eb3fb7c3ed609df81496
Prajwal P <prajwal.praveen@u-next.com>
2022-02-06 20:29:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import 'pdfmake/build/pdfmake'
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pdfmake-ng-warnings';
+  pdfmake: any;
+  constructor(){
+    this.pdfmake = window['pdfMake']
+  }
+
+  exportAsPdf() {
+    const sampleSheet = {
+      key1: 'value1',
+      key2: 'value2',
+      key3: 'value3',
+    }
+    this.pdfmake.fonts = {
+      Roboto: {
+        normal: 'Roboto-Regular.ttf',
+        bold: 'Roboto-Medium.ttf',
+        italics: 'Roboto-Italic.ttf',
+        bolditalics: 'Roboto-MediumItalic.ttf'
+      },
+    };
+    
+    const docDefinition: TDocumentDefinitions = {
+      content: [{
+        noWrap: false,
+        table: {
+          headerRows: 1,
+          body: [
+            [sampleSheet, sampleSheet]
+          ]
+        }
+      }],
+      defaultStyle: {
+        font: "Roboto",
+      }
+    };
+    this.pdfmake.createPdf(docDefinition, {}, this.pdfmake.fonts, pdfFonts.pdfMake.vfs).download(`sample.pdf`);
+  }
 }
